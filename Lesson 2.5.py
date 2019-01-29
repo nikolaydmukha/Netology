f2356ba2d97d00c17e0233be1594cd22b72b4e8a
nikolaydmukha <45710335+nikolaydmukha@users.noreply.github.com>
2019-01-29 19:17:41 +0300

    Lessom 2.5 has been added
    Lessom 2.5 has been added

---
+++ Lesson 2.5.py
@@ -0,0 +1,62 @@
+# Задача №1
+# Необходимо реализовать менеджер контекста, печатающий на экран:
+#
+# Время запуска кода в менеджере контекста;
+# Время окончания работы кода;
+# Сколько было потрачено времени на выполнение кода.
+# Задача №2
+# Придумать и написать программу, использующая менеджер контекста из задания 1.
+# Если придумать не получиться, использовать программу из предыдущих домашних работ.
+
+import datetime
+import time
+import random
+
+
+
+class ContextManagerShowTime:
+    def __init__(self, path_to_file):
+        print("Вход в менеджер контекста")
+        self.start = datetime.datetime.now()
+        self.start_sec = time.time()
+        self.path = path_to_file
+
+    def __enter__(self):
+        print(f"Начало работы {self.start}")
+        print("Sleeping just for 5 seconds...")
+        time.sleep(5)
+        self.file = open(self.path, "w")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = datetime.datetime.now()
+        self.end_sec = time.time()
+        duration = round(self.end_sec - self.start_sec)
+        print(f"Окончание работы {self.end}")
+        print(f"Общее время выполнения программы {duration} секунд")
+        self.file.close()
+        print("Выходим из менеджера контекста!")
+
+# Задача 1, 2
+path_to_file = "matrix.txt"
+with ContextManagerShowTime(path_to_file) as file:
+# Заполним данные для формирования матрицы
+    first = int(input("Введите первое число: "))
+    second = int(input("Введите второе число: "))
+    i = 0
+    matrix = []
+    while i <= first:
+        matrix_temp = []
+        j = 0
+        while j <= second:
+            matrix_temp.append(random.randrange(1, second))
+            j += 1
+        i += 1
+        matrix.append(matrix_temp)
+# Запишем матрицу в файл
+    for line in matrix:
+        temp_list = []
+        for element in line:
+            temp_list.append(str(element))
+        file.file.write(" ".join(temp_list) + "\n")
+
