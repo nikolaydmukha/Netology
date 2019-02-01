35091a19ad91e36518c95803b2c4ee9368da96e8
nikolaydmukha <45710335+nikolaydmukha@users.noreply.github.com>
2019-02-01 17:09:31 +0300

    Lesson 2.4 - only functions

--- Lesson 2.4(file).py
+++ Lesson 2.4(file).py
@@ -4,11 +4,12 @@
 
 from pprint import pprint
 
+
 # Читаем данные из файла
 def read_file(path_to_file):
     cook_book = {}
     list_ingredietnts = {}
-    with open(path_to_file, 'r') as f:
+    with open(path_to_file, 'r', encoding = "utf-8") as f:
         for line in f:
             if line.strip().isdigit():
                 quantity = int(line)
@@ -22,6 +23,7 @@ def read_file(path_to_file):
                 cook_book[name].append(list_ingredietnts)
     return cook_book
 
+
 # Формируем списко покупок
 def get_shop_list_by_dishes(dishes_list, count):
     path_to_file = "D:\Python\\Netology\\Netology\cook_book.txt"
@@ -34,21 +36,27 @@ def get_shop_list_by_dishes(dishes_list, count):
             for ingredient in cook_book[dish]:
                 if ingredient['ingredient_name'] not in shopping_list:
                     shopping_list[ingredient['ingredient_name']] = {'measure': ingredient['measure'],
-                                                                   'quantity': ingredient['quantity']*person_count}
+                                                                   'quantity': ingredient['quantity'] * count}
                 else:
-                    all_quatity = shopping_list[ingredient['ingredient_name']]['quantity'] + ingredient['quantity'] * person_count
+                    all_quatity = shopping_list[ingredient['ingredient_name']]['quantity'] + ingredient['quantity'] * count
                     shopping_list[ingredient['ingredient_name']]['quantity'] = all_quatity
     if shopping_list:
         pprint(shopping_list)
     else:
         print("Блюдо не найдено!")
 
-dishes_list = []
-while True:
-    dish = input("Введите название блюда или ENTER для продолжения: ")
-    if not dish:
-        person_count = int(input("Введите чилсо гостей: "))
-        get_shop_list_by_dishes(dishes_list, person_count)
-        break
-    else:
-        dishes_list.append(dish.lower())
+
+# Код для получения данных, необходимых программе
+def start_programm():
+    dishes_list = []
+    while True:
+        dish = input("Введите название блюда или ENTER для продолжения: ")
+        if not dish:
+            person_count = int(input("Введите чилсо гостей: "))
+            get_shop_list_by_dishes(dishes_list, person_count)
+            break
+        else:
+            dishes_list.append(dish.lower())
+
+
+start_programm()
