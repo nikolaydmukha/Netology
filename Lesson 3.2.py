17915472367c1772cf804d963ba01f9d715043f7
nikolaydmukha <dmukha@mail.ru>
2019-02-09 09:48:56 +0300

    Lesson 3.2 has been added

--- Lesson 3.2.py
+++ Lesson 3.2.py
@@ -1,33 +1,55 @@
+# Задача №1
+# Необходимо чтобы функцию переводчика принимала следующие параметры:
+#
+# Путь к файлу с текстом;
+# Путь к файлу с результатом;
+# Язык с которого перевести;
+# Язык на который перевести (по-умолчанию русский).
+# У вас есть 3 файла (DE.txt, ES.txt, FR.txt) с новостями на 3 языках: французском, испанском, немецком. Функция должна
+# взять каждый файл с текстом, перевести его на русский и сохранить результат в новом файле.
+
+
 import requests
+from pprint import pprint
 
 API_KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
-def translate_it(text, to_lang):
-    """
-    https://translate.yandex.net/api/v1.5/tr.json/translate ?
-    key=<API-ключ>
-     & text=<переводимый текст>
-     & lang=<направление перевода>
-     & [format=<формат текста>]
-     & [options=<опции перевода>]
-     & [callback=<имя callback-функции>]
-    :param to_lang:
-    :return:
-    """
 
+# Функция перевода текста, читаемого из файла. Результат перевода записывается в файл
+def translate_it(path_file_from, path_file_to, from_lang, to_lang="ru"):
+    # Я читаю файл построчно, чтобы получить текст в типе str(а не список) для передачи переводчику
+    with open(path_file_from, encoding="utf-8") as file:
+        text = ""
+        for line in file:
+            text += line
     params = {
         'key': API_KEY,
         'text': text,
-        'lang': 'ru-{}'.format(to_lang),
+        'lang': '{}-{}'.format(from_lang, to_lang),
     }
-
     response = requests.get(URL, params=params)
     json_ = response.json()
-    return ''.join(json_['text'])
+    # Запись в файл: переводчик возвращает текст в виде списка. В файл надо записать каждый элемент списка
+    with open(path_file_to, "w", encoding='utf-8') as f:
+        for member in json_["text"]:
+            f.write(member)
 
 
-# print(translate_it('В настоящее время доступна единственная опция — признак включения в ответ автоматически определенного языка переводимого текста. Этому соответствует значение 1 этого параметра.', 'no'))
+# Функция подготавливает необходимые для работы функции перевода данные
+def make_translate():
+    file_dir = 'D:\Python\\Netology\\Netology\\files\\'
+    dict_files = {
+        "DE.txt": "toDE.txt",
+        "ES.txt": "toES.txt",
+        "FR.txt": "toFR.txt",
+    }
+    for fr, to in dict_files.items():
+        path_file_from = file_dir + fr
+        path_file_to = file_dir + to
+        from_lang = fr.split(".")
+        translate_it(path_file_from, path_file_to, from_lang[0].lower())
 
 
-requests.post('http://requestb.in/10vc0zh1', json=dict(a='goo', b='foo'))
+make_translate()
+
