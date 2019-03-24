fa5836572040479dd43a569a0b60afe9a5f0b90c
nikolaydmukha <dmukha@mail.ru>
2019-03-24 23:22:41 +0300

    Rename parsded -> parsed

--- Adpy/Lesson 2.2/Lesson 2.2.py
+++ Adpy/Lesson 2.2/Lesson 2.2.py
@@ -2,6 +2,6 @@ from func import csv_writer, csv_processing
 
 
 if __name__ == "__main__":
-    csv_writer("phonebook_parsded.csv", csv_processing("phonebook.csv"))
+    csv_writer("phonebook_parsed.csv", csv_processing("phonebook.csv"))
     print("Контакты записной книги приведены к единому стандарту и записаны в файл!")
 
