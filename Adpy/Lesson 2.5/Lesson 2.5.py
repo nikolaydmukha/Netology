8ba297a36aa16550fea5d1ddf92f9875bb197c01
nikolaydmukha <45710335+nikolaydmukha@users.noreply.github.com>
2019-04-02 18:19:15 +0300

    Task 2

--- Adpy/Lesson 2.5/Lesson 2.5.py
+++ Adpy/Lesson 2.5/Lesson 2.5.py
@@ -17,6 +17,21 @@ def find_top3(query_years):
     return res_sort_df.head(3)
 
 
+def count_dinamics(query_years):
+    temp = []
+    for year in query_years:
+        filename = PATH + "\yob" + str(year) + ".txt"
+        print(filename)
+        df_temp = pd.read_csv(os.path.abspath(filename), sep=',', encoding='utf-8', header=None,
+                              names=["Gender", "Count"])
+        df_temp["Year"] = year
+        temp.append(df_temp)
+    res_df = pd.concat(temp).reset_index(drop=True)
+    res_sort_df = res_df.groupby(['Gender', 'Year']).sum().sort_values('Count', ascending=False)
+    return res_sort_df.head().unstack()
+
+
 if __name__ == "__main__":
-    print(find_top3([1880]))
-    print(find_top3([1900, 1950, 2000]))
+    # print(find_top3([1880]))
+    # print(find_top3([1900, 1950, 2000]))
+    print(count_dinamics([1900, 1950, 2000]))
