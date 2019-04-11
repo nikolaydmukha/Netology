188c592e3cf373caa599f9f8e46154a1704ed88e
nikolaydmukha <45710335+nikolaydmukha@users.noreply.github.com>
2019-04-11 07:35:55 +0300

    Diplom Adpy

---
+++ Adpy/Diplom/constants.py
@@ -0,0 +1,6 @@
+REQUEST_URL = "https://api.vk.com/method/"
+ACCESS_TOKEN = "ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae"
+
+# db_base =
+# db_paasword =
+# db_user =
---
+++ Adpy/Diplom/diplom.py
@@ -0,0 +1,8 @@
+import time
+import requests
+import sys
+import json
+import os
+import psycopg2
+
+
