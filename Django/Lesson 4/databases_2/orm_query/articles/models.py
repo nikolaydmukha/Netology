4c36c5f2a192d2d429b1fbfd4b65afd66429e97f
Nikolay Dmukha <dmukha@mail.ru>
2019-06-20 08:41:31 +0300

    Lesson 1.4(p.1, p.2, p.3)

--- Django/Lesson 4/databases_2/orm_query/articles/models.py
+++ Django/Lesson 4/databases_2/orm_query/articles/models.py
@@ -35,10 +35,10 @@ class Article(models.Model):
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
 
-
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ('-published_at',)
 
     def __str__(self):
         return self.title
--- Django/Lesson 4/databases_2/orm_query/articles/views.py
+++ Django/Lesson 4/databases_2/orm_query/articles/views.py
@@ -1,15 +1,13 @@
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Article
+from .models import Article, Author
 
 
 def articles_list(request):
     template_name = 'articles/news.html'
     context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
+    articles = Article.objects.all().select_related("author").only('author__name', 'genre', 'title', 'text')
+    context = {'articles': articles}
 
     return render(request, template_name, context)
