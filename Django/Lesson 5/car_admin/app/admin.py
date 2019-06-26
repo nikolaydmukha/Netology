0b4037378048a7da55cfc4789c4fcc4ec98c651e
Nikolay Dmukha <dmukha@mail.ru>
2019-06-26 22:41:15 +0300

    Lesson 5(p.1, p.2, p.3, p.4)

--- Django/Lesson 5/car_admin/app/admin.py
+++ Django/Lesson 5/car_admin/app/admin.py
@@ -5,11 +5,24 @@ from .forms import ReviewAdminForm
 
 
 class CarAdmin(admin.ModelAdmin):
-    pass
+    # список полей, отображаемых в админке
+    list_display = ['brand', 'model', 'get_reviews']
+    # список полей, по которым осуществляется поиск
+    search_fields = ['brand', 'model']
+    # филтьтр
+    list_filter = ('brand',)
+    # порядок сортировки
+    ordering = ('-id',)
 
 
 class ReviewAdmin(admin.ModelAdmin):
     form = ReviewAdminForm
+    # список полей, отображаемых в админке
+    list_display = ['car', 'title']
+    # список полей, по которым осуществляется поиск
+    search_fields = ['car__brand', 'car__model']
+    # порядок сортировки
+    ordering = ('-id',)
 
 
 admin.site.register(Car, CarAdmin)
--- Django/Lesson 5/car_admin/app/models.py
+++ Django/Lesson 5/car_admin/app/models.py
@@ -2,21 +2,34 @@ from django.db import models
 
 
 class Car(models.Model):
-    brand = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50, verbose_name='Марка')
+    model = models.CharField(max_length=50, verbose_name='Модель')
 
     def __str__(self):
         return f'{self.brand} {self.model}'
 
+    class Meta:
+        verbose_name = 'Автомобиль'
+        verbose_name_plural = 'Автомобили'
+
     def review_count(self):
         return Review.objects.filter(car=self).count()
 
+    def get_reviews(self):
+        return self.review_count()
+
+    get_reviews.short_description = "Всего отзывов"
+
 
 class Review(models.Model):
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    text = models.TextField()
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, verbose_name='Машина')
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Обзор')
 
     def __str__(self):
         return str(self.car) + ' ' + self.title
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
 
